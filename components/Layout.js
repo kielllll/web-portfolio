229c59ea19158400583692dbeabc2ababf34ae38
kielllll <eatarranza@gmail.com>
2021-05-28 23:31:30 +0800

    add: Initial setup of Material UI

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,20 @@
 import Footer from "./Footer";
 import NavBar from "./NavBar";
+import Head from "next/head";
 
 const Layout = ({ children }) => {
   return (
     <div>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </Head>
       <NavBar />
       {children}
       <Footer />
