5502474c102c9ee40d06710643e76946feef5d26
kielllll <eatarranza@gmail.com>
2021-05-29 00:28:47 +0800

    add: initial sections to Home page

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,39 @@ export default function Home() {
       <Head>
         <title>Home | Ezekiel Tarranza</title>
       </Head>
-      <h1>Home Page</h1>
+
+      <section>
+        <h1>
+          Hi there! I'm Ezekiel Tarranza, an aspirant Full-Stack Developer,
+          tech-enthusiast, and a potato!
+        </h1>
+        <p>
+          I find developing things fun! I explore, get in awe, and learn how to
+          do it. I do really love to work in groups as it is one of my forms of
+          socializing, making me a very good team-player!
+        </p>
+        <hr />
+      </section>
+
+      <section>
+        <h2>Featured Projects</h2>
+        <hr />
+      </section>
+
+      <section>
+        <h2>Experience</h2>
+        <hr />
+      </section>
+
+      <section>
+        <h2>Education</h2>
+        <hr />
+      </section>
+
+      <section>
+        <h2>Tech Stack</h2>
+        <hr />
+      </section>
     </div>
   );
 }
