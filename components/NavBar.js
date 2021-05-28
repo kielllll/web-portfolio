86dc2dc01d7fc9041bcbc9b2eb48dc7387a74be5
kielllll <eatarranza@gmail.com>
2021-05-28 23:58:55 +0800

    add: Initial styling of NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,25 +1,32 @@
 import Link from "next/link";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <nav>
-      <div>
-        <h1>
+    <AppBar color="transparent">
+      <Toolbar>
+        <div>
+          <h1>
+            <Link href="/">
+              <a>Zeke</a>
+            </Link>
+          </h1>
+        </div>
+        <div className={styles.links}>
           <Link href="/">
-            <a>Button</a>
+            <a className={styles.link}>Home</a>
           </Link>
-        </h1>
-      </div>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-      <Link href="/projects">
-        <a>Projects</a>
-      </Link>
-    </nav>
+          <Link href="/about">
+            <a className={styles.link}>About</a>
+          </Link>
+          <Link href="/projects">
+            <a className={styles.link}>Projects</a>
+          </Link>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 };
 
