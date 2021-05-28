303a3caf40953a4efbf5da8370cddd188a5d3eb2
kielllll <eatarranza@gmail.com>
2021-05-28 23:21:20 +0800

    add: initial about and projects page; removed default content for home

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>About | Ezekiel Tarranza</title>
+      </Head>
+      <h1>About Page</h1>
+    </div>
+  );
+}
---
+++ pages/projects.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Projects() {
+  return (
+    <div>
+      <Head>
+        <title>Projects | Ezekiel Tarranza</title>
+      </Head>
+      <h1>Projects Page</h1>
+    </div>
+  );
+}
