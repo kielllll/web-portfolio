80b9494a67ff233c00aebad572a57c8e1db09660
kielllll <eatarranza@gmail.com>
2021-05-29 20:45:55 +0800

    update: NavBar to disappear when scrolling down

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,32 +1,50 @@
 import Link from "next/link";
 import AppBar from "@material-ui/core/AppBar";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Slide from "@material-ui/core/Slide";
 import Toolbar from "@material-ui/core/Toolbar";
 import styles from "../styles/NavBar.module.css";
 
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
 const NavBar = () => {
   return (
-    <AppBar color="transparent">
-      <Toolbar>
-        <div>
-          <h1>
+    <HideOnScroll>
+      <AppBar color="transparent">
+        <Toolbar>
+          <div>
+            <h1>
+              <Link href="/">
+                <a>Zeke</a>
+              </Link>
+            </h1>
+          </div>
+          <div className={styles.links}>
             <Link href="/">
-              <a>Zeke</a>
+              <a className={styles.link}>Home</a>
             </Link>
-          </h1>
-        </div>
-        <div className={styles.links}>
-          <Link href="/">
-            <a className={styles.link}>Home</a>
-          </Link>
-          <Link href="/gear">
-            <a className={styles.link}>Gear</a>
-          </Link>
-          <Link href="/projects">
-            <a className={styles.link}>Projects</a>
-          </Link>
-        </div>
-      </Toolbar>
-    </AppBar>
+            <Link href="/gear">
+              <a className={styles.link}>Gear</a>
+            </Link>
+            <Link href="/projects">
+              <a className={styles.link}>Projects</a>
+            </Link>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </HideOnScroll>
   );
 };
 
