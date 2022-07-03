6679214d5a06e592a59065f58f78e78d4e55bfe0
kielllll <eatarranza@gmail.com>
2022-07-03 19:40:11 +0800

    update: experience description

--- data/data.js
+++ data/data.js
@@ -1,163 +1,163 @@
 export const featuredProjects = [
-    {
-        name: "Digital Knowmad",
-        description:
-            "A capstone project during my web development bootcamp with Zuitt.",
-        image: "/images/none.jpg",
-        repoLink:
-            "https://gitlab.com/tuitt/students/batch95/ezekiel-tarranza/csp2/app-client",
-        productionLink: "https://zeke-csp2-app-client.glitch.me/",
-        tags: ["html", "css", "javascript"],
-    },
-    // TODO: enable once Budget Wiser has been updated
-    // {
-    //     name: "Budget Wiser",
-    //     description:
-    //         "A capstone project during my web development bootcamp with Zuitt.",
-    //     image: "/images/none.jpg",
-    //     repoLink: "https://gitlab.com/kielllll/csp3-client",
-    //     productionLink: "https://csp3-client-iota.vercel.app/",
-    //     tags: ["next.js", "html", "css", "vercel"],
-    // },
-    {
-        name: "Peformance Analysis: TOTP Algorithm",
-        description: "A research project during my final year in college.",
-        image: "/images/none.jpg",
-        repoLink: "https://github.com/kielllll/thesis-ui",
-        productionLink: "https://thesis-totp.herokuapp.com/",
-        tags: ["nuxt.js", "html", "css", "heroku"],
-    },
-    {
-        name: "Web Portfolio",
-        description:
-            "My own web portfolio. Inspired by Benjanmin Carlson's Profile",
-        image: "/images/none.jpg",
-        repoLink: "https://github.com/kielllll/web-portfolio",
-        productionLink: "https://zeke-portfolio.vercel.app/",
-        tags: ["next.js", "html", "css", "vercel"],
-    },
-];
+  {
+    name: 'Digital Knowmad',
+    description:
+      'A capstone project during my web development bootcamp with Zuitt.',
+    image: '/images/none.jpg',
+    repoLink:
+      'https://gitlab.com/tuitt/students/batch95/ezekiel-tarranza/csp2/app-client',
+    productionLink: 'https://zeke-csp2-app-client.glitch.me/',
+    tags: ['html', 'css', 'javascript'],
+  },
+  // TODO: enable once Budget Wiser has been updated
+  // {
+  //     name: "Budget Wiser",
+  //     description:
+  //         "A capstone project during my web development bootcamp with Zuitt.",
+  //     image: "/images/none.jpg",
+  //     repoLink: "https://gitlab.com/kielllll/csp3-client",
+  //     productionLink: "https://csp3-client-iota.vercel.app/",
+  //     tags: ["next.js", "html", "css", "vercel"],
+  // },
+  {
+    name: 'Peformance Analysis: TOTP Algorithm',
+    description: 'A research project during my final year in college.',
+    image: '/images/none.jpg',
+    repoLink: 'https://github.com/kielllll/thesis-ui',
+    productionLink: 'https://thesis-totp.herokuapp.com/',
+    tags: ['nuxt.js', 'html', 'css', 'heroku'],
+  },
+  {
+    name: 'Web Portfolio',
+    description:
+      "My own web portfolio. Inspired by Benjanmin Carlson's Profile",
+    image: '/images/none.jpg',
+    repoLink: 'https://github.com/kielllll/web-portfolio',
+    productionLink: 'https://zeke-portfolio.vercel.app/',
+    tags: ['next.js', 'html', 'css', 'vercel'],
+  },
+]
 
 export const experiences = [
-    {
-        company: "Blue Liquid Designs",
-        role: "Software Engineer",
-        duration: "April 2022 - Present",
-        jobType: ["Full Time"],
-        description:
-            "Maintain and improve wordpress plugins.",
-    },
-    {
-        company: "Azoo Inc.",
-        role: "Web Developer",
-        duration: "June 2021 - April 2022",
-        jobType: ["Full Time"],
-        description:
-            "Maintain and improve web application, develop new features and components, and automation testing.",
-    },
-    {
-        company: "Summit Digital",
-        role: "Automations / Web Developer",
-        duration: "March 2021 - July 2021",
-        jobType: ["Part Time"],
-        description:
-            "Develop Lead Funnels and Automations through GoHighLevel CRM, LeadsHook, and Integration Platforms i.e Zapier, Pipedream.",
-    },
-    {
-        company: "itGenius",
-        role: "Software Engineer",
-        duration: "August 2019 - July 2021",
-        jobType: ["Full Time"],
-        description:
-            "Developing Web Applications, API Integrations, Automations, and Data Visualization using Google Data Studio",
-    },
-    {
-        company: "Concentrix",
-        role: "Technical Support Expert",
-        duration: "June 2019 - August 2019",
-        jobType: ["Full Time"],
-        description: "Provide seamless solutions to the consumers.",
-    },
-    {
-        company: "itGenius",
-        role: "Software Engineer",
-        duration: "October 2018 - February 2019",
-        jobType: ["Intern"],
-        description:
-            "Assist on Senior Engineers on the Web Development and API integrations.",
-    },
-];
+  {
+    company: 'Blue Liquid Designs',
+    role: 'Freelance Developer',
+    duration: 'April 2022 - Present',
+    jobType: ['Full Time'],
+    description: 'Currently learning on this great adventure!',
+  },
+  {
+    company: 'Azoo Inc.',
+    role: 'Freelance Developer',
+    duration: 'June 2021 - April 2022',
+    jobType: ['Full Time'],
+    description:
+      'I work as a developer in WASIMIL who handles mainly the front-end and sometimes the back-end. It is a hotel management software based in Japan that focuses on helping small to medium sized hotels to properly manage their hotel. My work on WASIMIL revolves around notification-like features, debugging, component development, and other frontend improvements.',
+  },
+  {
+    company: 'Summit Digital',
+    role: 'Freelance Developer',
+    duration: 'March 2021 - July 2021',
+    jobType: ['Part Time'],
+    description:
+      'I work as a developer in frontend and automations to work on our product in Summit Digital. Summit Digital is an Australian company that focuses on creating lead-generating automations that would help businesses specifically swim schools to gather leads. My role in the team is to generate automations using pipedream or zapier and leadshook for the lead pipeline',
+  },
+  {
+    company: 'itGenius',
+    role: 'Software Engineer',
+    duration: 'August 2019 - July 2021',
+    jobType: ['Full Time'],
+    description:
+      'I worked in ITGenius as a Developer mainly for custom reporting, scripts, and automations. I am exposed in doing the following task - Chatbot AI (Frontend & Backend), reports using Google Data Studio, custom scripts using Google Apps Script, and workflow automations using Zapier and Integromat.',
+  },
+  {
+    company: 'Concentrix',
+    role: 'Technical Support Expert',
+    duration: 'June 2019 - August 2019',
+    jobType: ['Full Time'],
+    description:
+      'I worked in Concentrix as Technical Support Representative in a Voice Account. This is where I honed my soft skills, specifically communication.',
+  },
+  {
+    company: 'itGenius',
+    role: 'Software Engineer',
+    duration: 'October 2018 - February 2019',
+    jobType: ['Intern'],
+    description:
+      'In my internship with ITGenius, I am exposed in doing the following task - fixing bugs in existing codebase, creating reports using Google Data Studio, and custom scripts using Google Apps Script.',
+  },
+]
 
 export const education = [
-    {
-        name: "Zuitt",
-        duration: "February 2021 - June 2021",
-        course: "Web Developer Bootcampee",
-        description:
-            "A web development bootcamp that uses the MERN stack. Has 3 capstone projects which really honed my skills.",
-    },
-    {
-        name: "University of Mindanao",
-        duration: "June 2016 - Present",
-        course: "BS in Computer Science",
-        description: "A lot of Mathematics, Theories, and Principles!",
-    },
-];
+  {
+    name: 'Zuitt',
+    duration: 'February 2021 - June 2021',
+    course: 'Web Developer Bootcampee',
+    description:
+      'In order to hone my skills to further improve my career. I have attended a bootcamp which focuses on MERN stack. We’ve had 3 capstone projects that we have developed.',
+  },
+  {
+    name: 'University of Mindanao',
+    duration: 'June 2016 - Present',
+    course: 'BS in Computer Science',
+    description: 'A lot of Mathematics, Theories, and Principles!',
+  },
+]
 
 export const hardware = [
-    {
-        type: "Processor",
-        name: "AMD Ryzen 5 3600",
-    },
-    {
-        type: "RAM",
-        name: "Team Elite 2 x 8GB",
-    },
-    {
-        type: "Graphics Card",
-        name: "Nvidia GTX 1650",
-    },
-    {
-        type: "SSD",
-        name: "Samsung M.2 500GB, AData SATA 2.5 265GB",
-    },
-    {
-        type: "HDD",
-        name: "Seagate 1TB",
-    },
-    {
-        type: "Primary Monitor",
-        name: "Asus VP259QGR 144hz 23.8in",
-    },
-    {
-        type: "Secondary Monitor",
-        name: "HP 23.8in",
-    },
-    {
-        type: "Keyboard",
-        name: "TM680 (Modded)",
-    },
-    {
-        type: "Mouse",
-        name: "ROG Stix Impact II Wireless",
-    },
-];
+  {
+    type: 'Processor',
+    name: 'AMD Ryzen 5 3600',
+  },
+  {
+    type: 'RAM',
+    name: 'Team Elite 2 x 8GB',
+  },
+  {
+    type: 'Graphics Card',
+    name: 'Nvidia GTX 1650',
+  },
+  {
+    type: 'SSD',
+    name: 'Samsung M.2 500GB, AData SATA 2.5 265GB',
+  },
+  {
+    type: 'HDD',
+    name: 'Seagate 1TB',
+  },
+  {
+    type: 'Primary Monitor',
+    name: 'Asus VP259QGR 144hz 23.8in',
+  },
+  {
+    type: 'Secondary Monitor',
+    name: 'HP 23.8in',
+  },
+  {
+    type: 'Keyboard',
+    name: 'TM680 (Modded)',
+  },
+  {
+    type: 'Mouse',
+    name: 'ROG Stix Impact II Wireless',
+  },
+]
 
 export const software = [
-    {
-        type: "IDE",
-        name: "Visual Studio Code / WebStorm / PhpStorm",
-    },
-    {
-        type: "API Tester",
-        name: "Postman / GraphQL Playground",
-    },
-    {
-        type: "Browser",
-        name: "Google Chrome / Brave",
-    },
-    {
-        type: "UI/UX Prototyping",
-        name: "Figma"
-    }
-];
+  {
+    type: 'IDE',
+    name: 'Visual Studio Code / WebStorm / PhpStorm',
+  },
+  {
+    type: 'API Tester',
+    name: 'Postman / GraphQL Playground',
+  },
+  {
+    type: 'Browser',
+    name: 'Google Chrome / Brave',
+  },
+  {
+    type: 'UI/UX Prototyping',
+    name: 'Figma',
+  },
+]
